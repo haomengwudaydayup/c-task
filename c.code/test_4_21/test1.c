1b5a207f67b5eb7bf35d21f5ff111ce914a80f5d
haomengwudaydayup <1543153682@qq.com>
2019-04-21 21:08:08 +0800

    test_4_21.c

--- c.code/test_4_21/test1.c
+++ c.code/test_4_21/test1.c
@@ -55,18 +55,18 @@ int main()
 	int i = 0;
 	int count = 0;
 
-	for(i=101; i<=200; i+=2)
+	for (i = 101; i <= 200; i += 2)
 	{
 		//printf("%d ", i);
 		//判断i是否为素数
 		int j = 0;
 		//sqrt(i)对i开平发
-		for(j=2; j<=sqrt(i); j++)
+		for (j = 2; j <= sqrt(i); j++)
 		{
-			if(i%j==0)
+			if (i%j == 0)
 				break;
 		}
-		if(j > sqrt(i))
+		if (j > sqrt(i))
 		{
 			printf("%d ", i);
 			count++;
