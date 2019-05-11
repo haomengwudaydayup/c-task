6729e4f4b68a089a909bd234c24352f4d962b0d7
haomengwudaydayup <1543153682@qq.com>
2019-05-11 23:25:56 +0800

    第五次作业

--- c.code/Project17/test_5_5.practice.cpp
+++ c.code/Project17/test_5_5.practice.cpp
@@ -1,131 +1,144 @@
-/*
-	数据结构-链表
-*/
+///*
+//	数据结构-链表
+//*/
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//typedef int Elemtype;
+//typedef int Status;
+//
+//typedef struct LNode {
+//	Elemtype data;
+//	struct LNode *next;
+//}LNode, * LinkList;
+//
+//void Create_NList(LinkList &L)
+//{
+//	L = (LinkList)malloc(sizeof(LNode));
+//	L->next = NULL;
+//	L->data = 0;
+//	LinkList head = L;
+//	int count = 0;//计数器
+//	printf("Input the node number: ");
+//	scanf_s("%d", &count);
+//	for (int i = count; i > 0; i--)
+//	{
+//		LinkList p = (LinkList)malloc(sizeof(LNode));//创建节点
+//		scanf_s("%d", &p->data);
+//		head->next = p;
+//		head = p;//尾插法-将新节点插到单链表的表尾上
+//		L->data++;//用头节点data存放表长
+//	}
+//	head->next = NULL;
+//	
+//	//{
+//	//	p->next = head->next;
+//	//	head->next = p->next;//头插法
+//	//}
+//	//return 1;
+//}
+//
+//void Input_Node(LinkList &L)
+//{
+//	LinkList head = L->next;
+//	while (head->next != NULL)
+//	{
+//		printf("%d  ", head->data);
+//		head = head->next;
+//	}
+//	printf("%d", head->data);
+//	
+//	printf("\n链表长度为%d\n", L->data);
+//}
+//
+//void Get_Node(LinkList L, int i)
+//{
+//	LinkList p = L->next;
+//	int j = 1;
+//	while (p && j < i)
+//	{
+//		p = p->next;
+//		j++;
+//	}//找到查找的位置
+//
+//	if (!p || j > i)
+//		exit(0);
+//
+//	printf("第%d号元素的值为%d\n", i,p->data);
+//}
+//
+//void Listinsert_N(LinkList &L, int i, Elemtype &e)
+//{
+//	LinkList head = L;
+//	int j = 0;
+//	while (head && j < i - 1)//找到添加的前一个节点
+//	{
+//		head = head->next;
+//		j++;
+//	}
+//	if (!head || j > i - 1)
+//		exit(0);
+//	LinkList p = (LinkList)malloc(sizeof(LNode));//创建添加节点
+//	p->data = e;//存储值
+//	p->next = head->next;
+//	head->next = p;
+//	L->data++;
+//}
+//
+//void Listdelete_N(LinkList &L, int i)
+//{
+//	int j = 0;
+//	LinkList head = L;
+//	while (head && j < i - 1)
+//	{
+//		head = head->next;
+//		j++;//找到i的位置
+//	}
+//	while (!head || j > i - 1)
+//		exit(0);
+//	LinkList p = head->next;
+//	head->next = p->next;
+//	L->data--;
+//	free(p);
+//}
+//int main()
+//{
+//	LinkList L;
+//	int i = 0;
+//	int e = 0;
+//	Create_NList(L);
+//	Input_Node(L);
+//
+//	printf("请输入您想查找的位置i>>");
+//	scanf_s("%d", &i);
+//	Get_Node(L, i);
+//	//Input_Node(L);
+//
+//	printf("请输入您想添加的位置和添加的值>");
+//	scanf_s("%d%d", &i, &e);
+//	Listinsert_N(L, i, e);
+//	Input_Node(L);
+//	
+//	printf("请输入您想删除的位置>");
+//	scanf_s("%d", &i);
+//	Listdelete_N(L, i);
+//	Input_Node(L);
+//
+//
+//	free(L);
+//	system("pause");
+//	return 0;
+//}
 #include<stdio.h>
 #include<stdlib.h>
+#include"add.h"
 
-typedef int Elemtype;
-typedef int Status;
+#pragma comment(lib,"testadd.lib")
 
-typedef struct LNode {
-	Elemtype data;
-	struct LNode *next;
-}LNode, * LinkList;
-
-void Create_NList(LinkList &L)
-{
-	L = (LinkList)malloc(sizeof(LNode));
-	L->next = NULL;
-	L->data = 0;
-	LinkList head = L;
-	int count = 0;//计数器
-	printf("Input the node number: ");
-	scanf_s("%d", &count);
-	for (int i = count; i > 0; i--)
-	{
-		LinkList p = (LinkList)malloc(sizeof(LNode));//创建节点
-		scanf_s("%d", &p->data);
-		head->next = p;
-		head = p;//尾插法-将新节点插到单链表的表尾上
-		L->data++;//用头节点data存放表长
-	}
-	head->next = NULL;
-	
-	//{
-	//	p->next = head->next;
-	//	head->next = p->next;//头插法
-	//}
-	//return 1;
-}
-
-void Input_Node(LinkList &L)
-{
-	LinkList head = L->next;
-	while (head->next != NULL)
-	{
-		printf("%d  ", head->data);
-		head = head->next;
-	}
-	printf("%d", head->data);
-	
-	printf("\n链表长度为%d\n", L->data);
-}
-
-void Get_Node(LinkList L, int i)
-{
-	LinkList p = L->next;
-	int j = 1;
-	while (p && j < i)
-	{
-		p = p->next;
-		j++;
-	}//找到查找的位置
-
-	if (!p || j > i)
-		exit(0);
-
-	printf("第%d号元素的值为%d\n", i,p->data);
-}
-
-void Listinsert_N(LinkList &L, int i, Elemtype &e)
-{
-	LinkList head = L;
-	int j = 0;
-	while (head && j < i - 1)//找到添加的前一个节点
-	{
-		head = head->next;
-		j++;
-	}
-	if (!head || j > i - 1)
-		exit(0);
-	LinkList p = (LinkList)malloc(sizeof(LNode));//创建添加节点
-	p->data = e;//存储值
-	p->next = head->next;
-	head->next = p;
-	L->data++;
-}
-
-void Listdelete_N(LinkList &L, int i)
-{
-	int j = 0;
-	LinkList head = L;
-	while (head && j < i - 1)
-	{
-		head = head->next;
-		j++;//找到i的位置
-	}
-	while (!head || j > i - 1)
-		exit(0);
-	LinkList p = head->next;
-	head->next = p->next;
-	L->data--;
-	free(p);
-}
 int main()
 {
-	LinkList L;
-	int i = 0;
-	int e = 0;
-	Create_NList(L);
-	Input_Node(L);
-
-	printf("请输入您想查找的位置i>>");
-	scanf_s("%d", &i);
-	Get_Node(L, i);
-	//Input_Node(L);
-
-	printf("请输入您想添加的位置和添加的值>");
-	scanf_s("%d%d", &i, &e);
-	Listinsert_N(L, i, e);
-	Input_Node(L);
-	
-	printf("请输入您想删除的位置>");
-	scanf_s("%d", &i);
-	Listdelete_N(L, i);
-	Input_Node(L);
-
-
-	free(L);
+	int a = 10, b = 20;
+	printf("%d\n", add(a, b));
 	system("pause");
 	return 0;
 }
---
+++ c.code/Project29/works.c
@@ -0,0 +1,171 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#if 0
+//第一道题
+void Print_table(int n)
+{
+	int i = 0, j = 0;
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("%2d * %2d = %2d", j, i, i*j);
+			printf(" ");
+		}
+		printf("\n");
+	}
+
+}
+
+int main()
+{
+	int num = 0;
+	printf("请输入您想打印的乘法口诀表行数和列数num>");
+	scanf_s("%d", &num);
+	Print_table(num);
+	system("pause");
+	return 0;
+}
+
+#elif 0
+//第二道题
+void Swap(int* pa, int*pb)
+{
+	int tmp = 0;
+	tmp = *pa;
+	*pa = *pb;
+	*pb = tmp;
+}
+
+int main()
+{
+	int a = 10, b = 20;
+	printf("未交换之前：a = %d, b = %d\n", a, b);
+	Swap(&a, &b);
+	printf("交换之后：a = %d, b = %d\n", a, b);
+	system("pause");
+	return 0;
+}
+
+#elif 0
+//第三道题
+void Is_leap_year(int year)
+{
+	if ((0 == year % 4 && 0 != year % 100) || (0 == year % 400))
+		printf("%d是闰年\n", year);
+	else
+	{
+		printf("%d不是闰年\n", year);
+	}
+	
+}
+
+int main()
+{
+	int year = 0;
+	printf("请输入您想判断的年份year>");
+	scanf_s("%d", &year);
+	Is_leap_year(year);
+	system("pause");
+	return 0;
+}
+
+#elif 1
+//第四道题
+//初始化数组
+void Print(int* a, int lengh)
+{
+	int i = 0;
+	for (i = 0; i < lengh; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
+void Init(int* a, int lengh)
+{
+	int i = 0;
+	int value = 0;
+	printf("请输入您存放的值\n");
+	for (i = 0; i < lengh; i++)
+	{
+		scanf_s("%d", a+i);
+	}
+	printf("初始化成功！\n");
+	Print(a, lengh);
+}
+//清空数组
+void empty(int* a, int lengh)
+{
+	//默认0为清空值
+	int i = 0;
+	for (i = 0; i < lengh; i++)
+	{
+		a[i] = 0;
+	}
+	printf("清空完毕!\n");
+	Print(a, lengh);
+}
+//逆序
+void reverse(int* a, int lengh)
+{j
+	int* p = a;
+	int* p_top = a;
+	int* p_end = a + lengh - 1;
+	for (p_top, p_end; p_top >= p_end; p_top++, p_end--)
+	{
+		int tmp = 0;
+		tmp = *p_top;
+		*p_top = *p_end;
+		*p_end = tmp;
+	}
+	Print(p, lengh);
+}
+void menu()
+{
+	printf("****** 1、初始化   *******\n");
+	printf("****** 2、逆序输出  *******\n");
+	printf("****** 3、清空数组  ******\n");
+	printf("****** 4、退出     ******\n");
+}
+
+int main()
+{
+	int a[10];
+	int choice = 0;
+	int lengh = sizeof(a) / sizeof(a[0]);
+	do
+	{
+		menu();
+		printf("请选择>");
+		scanf_s("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			//初始化数组
+			Init(a, lengh);
+			break;
+		case 2:
+			//逆序输出
+			reverse(a, lengh);
+			break;
+		case 3:
+			//清空
+			empty(a, lengh);
+			break;
+		case 4:
+			//退出
+			printf("退出！\n");
+			break;
+		default:
+			//非正确输入处理
+			printf("非法输入，请重新输入\n");
+			break;
+		}
+		printf("\n");
+
+	} while (choice != 4);
+	system("pause");
+	return 0;
+}
+#endif // 0
