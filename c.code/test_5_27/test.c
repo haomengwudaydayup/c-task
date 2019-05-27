c9cbf1b626cc025be30ac470b22d97558dd2a7d0
haomengwudaydayup <1543153682@qq.com>
2019-05-27 23:03:02 +0800

    test.txt

---
+++ c.code/test_5_27/test.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+
+int main()
+{
+	int i = 0;
+	int n = 0;
+	int j = 0;
+	int sum = 0;
+	int ret = 1;
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+	//	ret = 1;
+		if (i <= n)
+		{
+			if (ret == 1)
+			{
+				printf("%d\n", i);
+			}
+			assert(ret == 1);
+			}
+		for (j = 1; j <= i; j++)
+		{
+			ret = ret * j;
+		}
+		sum = sum + ret;
+	}
+	printf("%d\n", sum);
+	system("pause");
+	return 0;
+}
+
+
+
+
+
